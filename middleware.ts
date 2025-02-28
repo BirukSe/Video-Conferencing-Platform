d10a16f751a70260eb36dc8fa9dae35daacdc35a
biruke <93684688+BirukSe@users.noreply.github.com>
2025-02-28 04:17:30 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -9,10 +9,21 @@ const protectedRoute = createRouteMatcher([
   '/personal-room',
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
+export default clerkMiddleware(async (auth, req) => {
+  // Await the promise to get the resolved auth object
+  const authObj = await auth();
+
+  if (protectedRoute(req)) {
+    if (!authObj.userId) {
+      // If there is no userId, the user is not authenticated
+      return authObj.redirectToSignIn();
+    }
+  }
+
+  return; // Continue if the route doesn't need protection or user is authenticated
 });
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
