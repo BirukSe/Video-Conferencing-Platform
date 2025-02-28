856e6d371ba3e721467d893b2effaf4d64f2a27e
biruke <93684688+BirukSe@users.noreply.github.com>
2025-02-28 04:11:05 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,18 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// const protectedRoute = createRouteMatcher([
-//   '/',
-//   '/upcoming',
-//   '/meeting(.*)',
-//   '/previous',
-//   '/recordings',
-//   '/personal-room',
-// ]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (protectedRoute(req)) auth.protect();
-// });
-
-// export const config = {
-//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-import { clerkMiddleware } from '@clerk/nextjs/server';
+const protectedRoute = createRouteMatcher([
+  '/',
+  '/upcoming',
+  '/meeting(.*)',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+]);
 
 export default clerkMiddleware((auth, req) => {
-  // Just protect everything for now
-  auth.protect();
+  if (protectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ['/', '/upcoming', '/meeting(.*)', '/previous', '/recordings', '/personal-room'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
